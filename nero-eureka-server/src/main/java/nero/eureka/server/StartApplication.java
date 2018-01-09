d994e154cf788e9c5a1e41f67b69979f2d0b7341
nero <zhangnero@163.com>
2018-01-09 21:13:08 +0800

    添加spring-boot-starter-actuator依赖

--- nero-eureka-server/src/main/java/nero/eureka/server/StartApplication.java
+++ nero-eureka-server/src/main/java/nero/eureka/server/StartApplication.java
@@ -1,15 +1,15 @@
 package nero.eureka.server;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
 public class StartApplication {
 
-    public static void main(String[] args){
-        new SpringApplicationBuilder(StartApplication.class).web(true).run(args);
+    public static void main(String[] args) {
+        SpringApplication.run(StartApplication.class, args);
     }
-    
+
 }
